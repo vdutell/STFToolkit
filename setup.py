f3bea9f5243158cf6acfc6cf3d875b61975bd006
vdutell <vashadutell@gmail.com>
2019-12-01 19:51:09 +0000

    emacs extra files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='stftoolkit',
+      version='1.0',
+      description='Spatiotemporal Fourier Tools',
+      author='Vasha DuTell',
+      author_email='vashadutell@gmail.com',
+      url='https://github.com/vdutell/STFToolkit',
+      packages=find_packages()
+      )
